5494a19646eedbd7faf2b486acdd11be01fd80bc
Tomilola-ng <tee.o2809@gmail.com>
2024-03-15 18:29:52 +0100

    HERO-FIX

--- src/components/Hero/Images.tsx
+++ src/components/Hero/Images.tsx
@@ -1,15 +1,15 @@
 export default function Images() {
   return (
-    <div className="grid grid-cols-2">
-      <div className="relative p-6 pt-8">
-        <img
+    <div className="grid grid-cols-2 gap-x-5 p-2">
+      <div className="relative pt-8">
+        {/* <img
           src="/dot.jpeg"
           alt="dots"
           className="absolute mix-blend-multiply top-0 left-0 -z-0"
-        />
+        /> */}
         <img src="/girl1.jpeg" alt="girl1 picture" className="z-10" />
       </div>
-      <div className="pt-48 px-6">
+      <div className="pt-40">
         <img src="/girl2.jpeg" alt="girl2 picture" />
       </div>
     </div>
